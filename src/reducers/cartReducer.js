aa239a02c19e58d80e512004c476f42b8ac43a85
cristianqsanchez <cristianquintero316@gmail.com>
2023-05-08 14:54:37 -0500

    feat: :sparkles: create cart reducer actions

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,41 @@
+export const cartInitialState = []
+
+export const CART_ACTION_TYPES = {
+  ADD_TO_CART: 'ADD_TO_CART',
+  REMOVE_FROM_CART: 'REMOVE_FROM_CART',
+  CLEAR_CART: 'CLEAR_CART'
+}
+
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTION_TYPES.ADD_TO_CART]: (state, action) => {
+    const { id } = action.payload
+    const productInCartIndex = state.findIndex(item => item.id === id)
+    if (productInCartIndex >= 0) {
+      const newState = structuredClone(state)
+      newState[productInCartIndex].quantity += 1
+      return newState
+    }
+    const newState = [
+      ...state,
+      {
+        ...action.payload,
+        quantity: 1
+      }
+    ]
+    return newState
+  },
+  [CART_ACTION_TYPES.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = action.payload
+    const newState = state.filter(item => item.id !== id)
+    return newState
+  },
+  [CART_ACTION_TYPES.CLEAR_CART]: () => {
+    return []
+  }
+}
+
+export const cartReducer = (state, action) => {
+  const { type: actionType } = action
+  const updateState = UPDATE_STATE_BY_ACTION[actionType]
+  return updateState ? updateState(state, action) : state
+}
