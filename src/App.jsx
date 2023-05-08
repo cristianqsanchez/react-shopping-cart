4178957cad3d77b4c8eb8035ad42a036aa29e9ad
cristianqsanchez <cristianquintero316@gmail.com>
2023-05-08 15:00:02 -0500

    feat: :sparkles: bootstrap app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Cart from './components/Cart'
+import Header from './components/Header'
+import ProductList from './components/ProductList'
+import { CartProvider } from './context/CartContext'
+import useFilters from './hooks/useFilters'
+import { products } from './mocks/products.json'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const { filterProducts } = useFilters()
+  const filteredProducts = filterProducts(products)
 
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <CartProvider>
+      <Header />
+      <Cart />
+      <ProductList products={filteredProducts} />
+    </CartProvider>
   )
 }
 
